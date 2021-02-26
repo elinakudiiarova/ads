6842b319c2c977d819b9fbbf9dfaae021f084971
Elina Kudiiarova <elina.kudiyarova@gmail.com>
2021-02-26 16:14:15 +0200

    adding case if we should add elem between nodes

--- Lab1_SortedLinkedList/Program.cs
+++ Lab1_SortedLinkedList/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
 namespace Lab1_SortedLinkedList
 {
     class Program
@@ -21,15 +21,37 @@ namespace Lab1_SortedLinkedList
             }
             else
             {
-                if (Comparer<T>.Default.Compare(myList.First.Value, elem) == -1 )
-                {
+                //if (Comparer<T>.Default.Compare(myList.First.Value, elem) == -1 )
+                //{
 
-                }
-                else
-                {
-                    T trail = myList.FindLast(listEl => listEl < elem).Value;
+                //}
+                //else
+                //{
+                //    T trail = myList.FindLast(T listEl => Comparer<T>.Default.Compare(listEl, elem) == -1).Value;
+                //    myList.AddAfter(trail, elem);
 
+                //}
+                var currentNode = myList.First.Next;
+                LinkedListNode<T> prevNode = myList.First;
+                while (currentNode != null){
+                    if(Comparer<T>.Default.Compare(currentNode.Value, prevNode.Value) == -1)
+                    {
+                        myList.AddBefore(prevNode, elem);
+                        break;
+                    }
+                    prevNode = currentNode;
+                    currentNode = currentNode.Next;
                 }
+                
+                //if (trail == null)
+                //{
+                //    myList.AddFirst(elem);
+                //}
+                //else
+                //{
+                    
+                //    myList.AddAfter(trail, elem);
+                //}
             }
         }
     }
