db68c0dbf52051f7a2f8a8132556a8fc3ec60668
Elina Kudiiarova <elina.kudiyarova@gmail.com>
2021-02-26 17:01:40 +0200

    add class that creates sorted linked list

--- Lab1_SortedLinkedList/Program.cs
+++ Lab1_SortedLinkedList/Program.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 namespace Lab1_SortedLinkedList
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var SorLinList = new MySortedLinkedList<int>();
+            while (true)
+            {
+                var elem = Console.ReadLine();
+                if (elem == "stop") break;
+                SorLinList.AddElement(int.Parse(elem));
+            }
+ 
         }
     }
     class MySortedLinkedList<T>
     {
-        LinkedList<T> myList; // creating linked list
+        LinkedList<T> myList = new LinkedList<T>(); // creating linked list
 
-        void AddElement(T elem)
+        public void AddElement(T elem)
         {
             if (myList.Count == 0)
             {
@@ -21,37 +28,21 @@ namespace Lab1_SortedLinkedList
             }
             else
             {
-                //if (Comparer<T>.Default.Compare(myList.First.Value, elem) == -1 )
-                //{
-
-                //}
-                //else
-                //{
-                //    T trail = myList.FindLast(T listEl => Comparer<T>.Default.Compare(listEl, elem) == -1).Value;
-                //    myList.AddAfter(trail, elem);
-
-                //}
-                var currentNode = myList.First.Next;
-                LinkedListNode<T> prevNode = myList.First;
-                while (currentNode != null){
-                    if(Comparer<T>.Default.Compare(currentNode.Value, prevNode.Value) == -1)
+                var currentNode = myList.First;
+                while (currentNode != null)
+                {
+                    if (Comparer<T>.Default.Compare(currentNode.Value, elem) == -1)
                     {
-                        myList.AddBefore(prevNode, elem);
+                        myList.AddBefore(currentNode, elem);
                         break;
                     }
-                    prevNode = currentNode;
+                    if (currentNode.Next == null)
+                    {
+                        myList.AddLast(elem);
+                    }
                     currentNode = currentNode.Next;
+
                 }
-                
-                //if (trail == null)
-                //{
-                //    myList.AddFirst(elem);
-                //}
-                //else
-                //{
-                    
-                //    myList.AddAfter(trail, elem);
-                //}
             }
         }
     }
